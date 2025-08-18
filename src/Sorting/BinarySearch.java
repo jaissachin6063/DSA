687f1f2e4b1caed781af223b9be197a18e10bf22
sachin <sachinjais6063@gmail.com>
2025-08-18 18:23:03 +0530

    DSA Code

---
+++ src/Sorting/BinarySearch.java
@@ -0,0 +1,34 @@
+package Sorting;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int nums[] = {6, 8,  9, 2, 4, 5};
+        int n = nums.length;
+        System.out.println("Before Sorting: ");
+        for (int num: nums){
+            System.out.print(num + " ");
+        }
+        for (int i = 0; i < n; i++) {
+            for(int j=0; j<n-i-1; j++){
+                if (nums[j+1] < nums[j]){
+                    int temp = nums[j+1];
+                    nums[j+1] =nums[j];
+                    nums[j] = temp;
+
+
+                }
+            }
+            System.out.println();
+            for (int num: nums){
+                System.out.print(num + " ");
+            }
+        }
+        System.out.println();
+        System.out.println("After Sorting: ");
+        for (int num: nums){
+            System.out.print(num + " ");
+        }
+
+
+    }
+}
